b61a2181bda313c39f88ea95c3a28f09b18361a7
anderssonderby <50653739+anderssonderby@users.noreply.github.com>
2022-03-26 00:25:23 +0100

    Create Empty Spring Boot project

---
+++ src/main/java/anderssonderby/bestseller/automatching/AutomatchingApplication.java
@@ -0,0 +1,12 @@
+package anderssonderby.bestseller.automatching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatchingApplication.class, args);
+	}
+}
---
+++ src/test/java/anderssonderby/bestseller/automatching/AutomatchingApplicationTests.java
@@ -0,0 +1,13 @@
+package anderssonderby.bestseller.automatching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatchingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
